a7c3313444e782d7a11d7db60edea5b088c58dee
harlin <1855418+harlinw@users.noreply.github.com>
2020-11-09 15:48:01 -0800

    notes test

--- src/use-midi-notes.ts
+++ src/use-midi-notes.ts
@@ -8,7 +8,7 @@ export const useMIDINotes = (input: Input, filter: MIDIFilter = {}) => {
   const [notes, setNotes] = useState<MIDINote[]>([]);
   const value = useMIDINote(input, filter);
   useEffect(() => {
-    if (!input) return;
+    if (!input || !value) return;
     if (value.on) setNotes([...notes, value]);
     else setNotes(notes.filter((n) => n.note !== value.note)); // Note off, remove note from array (maybe check for channel?)
   }, [input, value]);
